086232eb81d166ad257275c470bcdd120a6f1b27
v0ld3mort <105578675+v0ld3mort@users.noreply.github.com>
2022-05-15 16:39:37 +1000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 apt -y install sipcalc nmap
-wget https://raw.githubusercontent.com/systematicat/hack-captive-portals/master/hack-captive.sh
+wget https://raw.githubusercontent.com/v0ld3mort/effective-octo-barnacle/main/hack-captive.sh
 chmod u+x hack-captive.sh
 rm download.sh
 sudo ./hack-captive.sh
