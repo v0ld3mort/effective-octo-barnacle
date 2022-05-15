2664458da767779320c1b44b0583aba6aba1e899
v0ld3mort <105578675+v0ld3mort@users.noreply.github.com>
2022-05-15 16:27:33 +1000

    Created download.sh
    Initial commit

---
+++ download.sh
@@ -0,0 +1,5 @@
+apt -y install sipcalc nmap
+wget https://raw.githubusercontent.com/systematicat/hack-captive-portals/master/hack-captive.sh
+chmod u+x hack-captive.sh
+rm download.sh
+sudo ./hack-captive.sh
