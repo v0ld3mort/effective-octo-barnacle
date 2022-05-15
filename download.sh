dcde8a570e898f32728ae3b6979d2be762b34e51
v0ld3mort <105578675+v0ld3mort@users.noreply.github.com>
2022-05-15 20:16:06 +1000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-apt -y install sipcalc nmap isc-dhcp-server dhcpd lighttpd beef-xss bettercap hostapd-wpe hostapd
+apt -y install sipcalc nmap isc-dhcp-server dhcpd lighttpd beef-xss bettercap hostapd-wpe hostapd mdk4
 wget https://raw.githubusercontent.com/v0ld3mort/effective-octo-barnacle/main/hack-captive.sh
 chmod u+x hack-captive.sh
 rm download.sh
